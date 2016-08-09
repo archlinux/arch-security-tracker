762bfcdaf9772c2f988dff131fd6e0af8b3cc1d4
anthraxx <levente@leventepolyak.net>
2016-08-09 11:00:05 +0200

    advisory: adding unhandled open advisories to view

--- app/view/advisory.py
+++ app/view/advisory.py
@@ -1,14 +1,16 @@
 from flask import render_template, flash, redirect
 from app import app, db
-from app.model import CVEGroupPackage, Advisory
-from app.model.enum import Publication
+from app.model import CVEGroup, CVEGroupPackage, Advisory
+from app.model.enum import Publication, Status, Affected
 from collections import OrderedDict
+from sqlalchemy import func
 
 
 @app.route('/advisory')
 def advisory():
-    entries = (db.session.query(Advisory, CVEGroupPackage)
-               .join(CVEGroupPackage)
+    entries = (db.session.query(Advisory, CVEGroupPackage, CVEGroup)
+               .join(CVEGroupPackage).join(CVEGroup)
+               .group_by(CVEGroupPackage.id)
                .order_by(Advisory.created.desc())).all()
 
     scheduled = list(filter(lambda item: item[0].publication == Publication.scheduled, entries))
@@ -17,18 +19,27 @@ def advisory():
     published = list(filter(lambda item: item[0].publication == Publication.published, entries))
     published = sorted(published, key=lambda item: item[0].created, reverse=True)
 
+    unhandled = (db.session.query(CVEGroupPackage, CVEGroup)
+                 .join(CVEGroup)
+                 .outerjoin(Advisory)
+                 .filter(CVEGroup.advisory_qualified)
+                 .filter(CVEGroup.status == Status.fixed)
+                 .group_by(CVEGroupPackage.id)
+                 .having(func.count(Advisory.id) == 0)
+                 .order_by(CVEGroupPackage.id)).all()
+
     monthly_published = OrderedDict()
     for item in published:
         advisory = item[0]
-        package = item[1]
         month = advisory.created.strftime('%B %Y')
         if month not in monthly_published:
             monthly_published[month] = []
-        monthly_published[month].append((advisory, package))
+        monthly_published[month].append(item)
 
     entries = {
         'scheduled': scheduled,
-        'published': monthly_published
+        'published': monthly_published,
+        'unhandled': unhandled
     }
     return render_template('advisory.html',
                            title='Advisories',
