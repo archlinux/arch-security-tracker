42b655e6fdaa03911ae5db4ae66c8fd2776071e6
Santiago Torres <torresariass@gmail.com>
2016-08-28 13:56:43 -0400

    style: Add max-length constants to CVE/AVG models
    Currently, CVE and AVG models contain description and notes fields with
    a maximum length defined as a magical number. Given that these values
    will be used throughout the codebase, moving them to a constant within
    the same class will avoid repeating the magic number everywhere else.
    
    Add a constant for the CVEGroup and CVE models that describe the maximum
    length of the notes and description fields.

--- app/model/cve.py
+++ app/model/cve.py
@@ -6,12 +6,16 @@ cve_id_regex = r'^(CVE\-\d{4}\-\d+)$'
 
 
 class CVE(db.Model):
+
+    NOTES_LENGTH = 4096
+    DESCRIPTION_LENGTH = 4096
+
     __tablename__ = 'cve'
     id = db.Column(db.String(15), index=True, unique=True, primary_key=True)
-    description = db.Column(db.String(4096))
+    description = db.Column(db.String(DESCRIPTION_LENGTH))
     severity = db.Column(Severity.as_type(), nullable=False, default=Severity.unknown)
     remote = db.Column(Remote.as_type(), nullable=False, default=Remote.unknown)
-    notes = db.Column(db.String(4096))
+    notes = db.Column(db.String(NOTES_LENGTH))
 
     def __repr__(self):
         return '{}'.format(self.id)
