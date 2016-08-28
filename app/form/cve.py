82a347492540c375090184833c0bc254ed4277c7
Santiago Torres <torresariass@gmail.com>
2016-08-27 21:41:06 -0400

    style: cve: add maxlength to description and notes
    Maximum length validation for the description and notes field for CVE
    and AVG is done on the backend. As of now, the application truncates any
    description that exceeds the LENGTH definition. Although this is the
    right way to ensure no excess data is given, descriptions and notes may
    end truncated with prior notice to the user, which may be undesirable.
    
    Add a maxlength attribute to the textarea fields, as well as a validator
    for the cve and avg form and form template.

--- app/form/cve.py
+++ app/form/cve.py
@@ -1,14 +1,15 @@
 from .base import BaseForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Optional
 from app.form.validators import ValidIssue
+from app.model.cve import CVE
 from app.model.enum import Severity, Remote
 
 
 class CVEForm(BaseForm):
     cve = StringField(u'CVE', validators=[DataRequired(), ValidIssue()])
-    description = TextAreaField(u'Description', validators=[])
+    description = TextAreaField(u'Description', validators=[Optional(), Length(max=CVE.DESCRIPTION_LENGTH)])
     severity = SelectField(u'Severity', choices=[(e.name, e.label) for e in [*Severity]], validators=[])
     remote = SelectField(u'Remote', choices=[(e.name, e.label) for e in [*Remote]], validators=[])
-    notes = TextAreaField(u'Notes', validators=[])
+    notes = TextAreaField(u'Notes', validators=[Length(max=CVE.NOTES_LENGTH)])
     submit = SubmitField(u'submit')
