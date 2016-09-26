fd4ff0dd988188102a207fd8678d1b1677b24f02
anthraxx <levente@leventepolyak.net>
2016-09-26 19:54:16 +0200

    advisory: implemented basic advisory draft generation
    This is part of #20.

--- app/view/show.py
+++ app/view/show.py
@@ -2,12 +2,14 @@ from flask import render_template
 from sqlalchemy import and_
 from app import app, db
 from app.model import CVE, CVEGroup, CVEGroupEntry, CVEGroupPackage, Advisory
-from app.model.enum import Publication, Status
+from app.model.enum import Publication, Status, Remote
 from app.model.cve import cve_id_regex
 from app.model.cvegroup import vulnerability_group_regex, pkgname_regex
+from app.model.advisory import advisory_regex
 from app.form.advisory import AdvisoryForm
 from app.view.error import not_found
 from app.pacman import get_pkg
+from app.util import chunks
 from collections import defaultdict
 
 
@@ -135,3 +137,70 @@ def show_package(pkgname):
     return render_template('package.html',
                            title='{}'.format(pkgname),
                            package=package)
+
+
+@app.route('/advisory/<regex("{}"):advisory_id>/raw'.format(advisory_regex[1:-1]), methods=['GET'])
+@app.route('/<regex("{}"):advisory_id>/raw'.format(advisory_regex[1:-1]), methods=['GET'])
+def show_advisory_raw(advisory_id):
+    result = show_advisory(advisory_id, raw=True)
+    if isinstance(result, tuple):
+        return result
+    return result, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+
+@app.route('/advisory/<regex("{}"):advisory_id>'.format(advisory_regex[1:]), methods=['GET'])
+@app.route('/<regex("{}"):advisory_id>'.format(advisory_regex[1:]), methods=['GET'])
+def show_advisory(advisory_id, raw=False):
+    entries = (db.session.query(Advisory, CVEGroup, CVEGroupPackage, CVE)
+               .filter(Advisory.id == advisory_id)
+               .join(CVEGroupPackage).join(CVEGroup).join(CVEGroupEntry).join(CVE)
+               .order_by(CVE.id)
+               ).all()
+    if not entries:
+        return not_found()
+
+    advisory = entries[0][0]
+    group = entries[0][1]
+    package = entries[0][2]
+    issues = [issue for (advisory, group, package, issue) in entries]
+    issues = sorted(issues, key=lambda issue: issue.issue_type)
+    issues = sorted(issues, key=lambda issue: issue.severity)
+
+    remote = any([issue.remote is Remote.remote for issue in issues])
+    issues_listing_formatted = (('\n{}'.format(' ' * len('CVE-ID  : ')))
+                                .join(list(map(' '.join, chunks([issue.id for issue in issues], 4)))))
+    link = 'https://wiki.archlinux.org/index.php/CVE'
+    upstream_released = group.affected.split('-')[0] != group.fixed.split('-')[0]
+    upstream_version = group.fixed.split('-')[0]
+    if ':' in upstream_version:
+        upstream_version = upstream_version[upstream_version.index(':') + 1:]
+    unique_issue_types = []
+    for issue in issues:
+        if issue.issue_type not in unique_issue_types:
+            unique_issue_types.append(issue.issue_type)
+
+    raw_asa = render_template('asa.txt',
+                              title='{}'.format(advisory_id),
+                              advisory=advisory,
+                              group=group,
+                              package=package,
+                              issues=issues,
+                              remote=remote,
+                              issues_listing_formatted=issues_listing_formatted,
+                              link=link,
+                              upstream_released=upstream_released,
+                              upstream_version=upstream_version,
+                              unique_issue_types=unique_issue_types)
+    if raw:
+        return raw_asa
+
+    raw_asa = '\n'.join(raw_asa.split('\n')[2:])
+    for issue in issues:
+        raw_asa = raw_asa.replace(' {}'.format(issue.id), ' <a href="/{0}">{0}</a>'.format(issue.id))
+    raw_asa = raw_asa.replace(package.pkgname, '<a href="/package/{0}">{0}</a>'.format(package.pkgname))
+
+    return render_template('asa.html',
+                           title='{}'.format(advisory_id),
+                           advisory=advisory,
+                           package=package,
+                           raw_asa=raw_asa)
