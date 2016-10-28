7f2504c9bd22b052dcd3b6397c160c3bb6d0c76a
anthraxx <levente@leventepolyak.net>
2016-10-28 14:40:54 +0200

    config: avoid print all queries to stdout by default

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = path.abspath(path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = path.join(basedir, 'db_repository')
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 CSRF_ENABLED = True
