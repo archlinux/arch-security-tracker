ef7b6a594b020a3e0c334093314b6171cd7dee98
anthraxx <levente@leventepolyak.net>
2016-10-30 22:20:06 +0100

    advisory: validate reference content against actual advisory

---
+++ app/advisory.py
@@ -0,0 +1,59 @@
+from re import sub
+from requests import get
+from html import unescape
+
+
+def advisory_fetch_from_mailman(url):
+    try:
+        response = get(url)
+        if 200 != response.status_code:
+            return None
+        asa = unescape(sub('</?A[^<]*?>', '', response.text))
+        start = '<PRE>'
+        start_marker = '{}Arch Linux Security Advisory'.format(start)
+        end = '\n-------------- next part --------------'
+        asa = asa[asa.index(start_marker) + len(start):asa.index(end)]
+        return asa.strip()
+    except Exception:
+        return None
+
+
+def advisory_get_impact_from_text(advisory):
+    start = '\nImpact\n======\n\n'
+    end = '\n\nReferences\n'
+    if start not in advisory or end not in advisory:
+        return None
+    start_index = advisory.index(start)
+    end_index = advisory.index(end)
+    impact = advisory[start_index + len(start):end_index]
+    impact = sub('([^.\n])\\n', '\\1 ', impact)
+    return impact
+
+
+def advisory_get_workaround_from_text(advisory):
+    start = '\nWorkaround\n==========\n\n'
+    end = '\n\nDescription\n'
+    if start not in advisory or end not in advisory:
+        return None
+    start_index = advisory.index(start)
+    end_index = advisory.index(end)
+    workaround = advisory[start_index + len(start):end_index]
+    if 'None.' == workaround:
+        return None
+    return workaround
+
+
+def advisory_extend_html(advisory, issues, package):
+    for issue in issues:
+        advisory = advisory.replace(' {}'.format(issue.id), ' <a href="/{0}">{0}</a>'.format(issue.id))
+    advisory = advisory.replace(' {}'.format(package.pkgname), ' <a href="/package/{0}">{0}</a>'.format(package.pkgname))
+    advisory = advisory.replace('"{}'.format(package.pkgname), '"<a href="/package/{0}">{0}</a>'.format(package.pkgname))
+    return advisory
+
+
+def advisory_extend_model_from_advisory_text(advisory):
+    if not advisory.content:
+        return advisory
+    advisory.impact = advisory_get_impact_from_text(advisory.content)
+    advisory.workaround = advisory_get_workaround_from_text(advisory.content)
+    return advisory
