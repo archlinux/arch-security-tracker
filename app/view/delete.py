1e4ba543baed46da58ac028d66ae1005c2fe8f62
anthraxx <levente@leventepolyak.net>
2016-11-18 23:41:19 +0200

    issue: delete view for security_team privileged users

--- app/view/delete.py
+++ app/view/delete.py
@@ -4,7 +4,9 @@ from app.user import security_team_required
 from app.form.confirm import ConfirmForm
 from app.model import CVEGroup, CVE, CVEGroupPackage, CVEGroupEntry, Advisory
 from app.model.cvegroup import vulnerability_group_regex
+from app.model.cve import cve_id_regex
 from app.view.error import not_found, forbidden
+from collections import defaultdict
 
 
 @app.route('/group/<regex("{}"):avg>/delete'.format(vulnerability_group_regex[1:-1]), methods=['GET', 'POST'])
@@ -54,4 +56,70 @@ def delete_group(avg):
     db.session.delete(group)
     db.session.commit()
     flash('Deleted {}'.format(group))
+    return redirect('/')
+
+
+@app.route('/issue/<regex("{}"):issue>/delete'.format(cve_id_regex[1:-1]), methods=['GET', 'POST'])
+@app.route('/<regex("{}"):issue>/delete'.format(cve_id_regex[1:-1]), methods=['GET', 'POST'])
+@security_team_required
+def delete_issue(issue):
+    entries = (db.session.query(CVE, CVEGroup, CVEGroupPackage, Advisory)
+               .filter(CVE.id == issue)
+               .outerjoin(CVEGroupEntry).outerjoin(CVEGroup).outerjoin(CVEGroupPackage)
+               .outerjoin(Advisory, Advisory.group_package_id == CVEGroupPackage.id)
+               .order_by(CVEGroup.created.desc()).order_by(CVEGroupPackage.pkgname)).all()
+    if not entries:
+        return not_found()
+
+    issue = entries[0][0]
+    advisories = set()
+    groups = set()
+    group_packages = defaultdict(set)
+    for cve, group, pkg, advisory in entries:
+        if group:
+            groups.add(group)
+            group_packages[group].add(pkg.pkgname)
+        if advisory:
+            advisories.add(advisory)
+
+    if advisories:
+        return forbidden()
+
+    group_entries = (db.session.query(CVEGroup, CVE)
+                     .filter(CVEGroup.id.in_([group.id for group in groups]))
+                     .join(CVEGroupEntry).join(CVE)
+                     .order_by(CVE.id.desc())).all()
+
+    group_issues = defaultdict(set)
+    for group, cve in group_entries:
+        group_issues[group].add(cve)
+
+    groups = sorted(groups, key=lambda item: item.created, reverse=True)
+    groups = sorted(groups, key=lambda item: item.status)
+    group_packages = dict(map(lambda item: (item[0], sorted(item[1])), group_packages.items()))
+
+    form = ConfirmForm()
+    title = 'Delete {}'.format(issue)
+    if not form.validate_on_submit():
+        return render_template('form/delete_cve.html',
+                               title=title,
+                               heading=title,
+                               form=form,
+                               issue=issue,
+                               groups=groups,
+                               group_packages=group_packages,
+                               group_issues=group_issues)
+
+    if not form.confirm.data:
+        return redirect('/{}'.format(issue))
+
+    # delete groups that only contain this issue
+    for group, issues in group_issues.items():
+        if 0 == len(list(filter(lambda e: e.id != issue.id, issues))):
+            flash('Deleted {}'.format(group))
+            db.session.delete(group)
+
+    db.session.delete(issue)
+    db.session.commit()
+    flash('Deleted {}'.format(issue))
     return redirect('/')
