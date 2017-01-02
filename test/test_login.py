6e3dff3c8a6ad7496ba1b2d4253aef081bc55d74
anthraxx <levente@leventepolyak.net>
2017-01-02 15:57:57 +0100

    test: adding create_user and logged_in decorators
    This makes it lot easier to setup data and requirements for test
    cases.

--- test/test_login.py
+++ test/test_login.py
@@ -1,60 +1,37 @@
-import pytest
-
 from werkzeug.exceptions import Unauthorized
 from flask import url_for
 from flask_login import current_user
 
-from .conftest import create_user, USERNAME
+from .conftest import assert_logged_in, assert_not_logged_in, logged_in, create_user, DEFAULT_USERNAME
 from config import TRACKER_PASSWORD_LENGTH_MIN
 from app.form.login import ERROR_INVALID_USERNAME_PASSWORD, ERROR_ACCOUNT_DISABLED
 
 
-@pytest.fixture(autouse=True)
-def setup(db):
-    create_user(db)
-
-
-@pytest.fixture
-def assert_logged_in(response, status_code=200):
-    assert status_code == response.status_code
-    assert b'logout' in response.data
-    assert b'login' not in response.data
-    assert current_user.is_authenticated
-
-
-@pytest.fixture
-def assert_not_logged_in(response, status_code=200):
-    assert status_code == response.status_code
-    assert b'logout' not in response.data
-    assert b'login' in response.data
-    assert not current_user.is_authenticated
-
-
-def test_login_success(client):
+@create_user
+def test_login_success(db, client):
     resp = client.post(url_for('login'), follow_redirects=True,
-                       data=dict(username=USERNAME, password=USERNAME))
+                       data=dict(username=DEFAULT_USERNAME, password=DEFAULT_USERNAME))
     assert_logged_in(resp)
-    assert USERNAME == current_user.name
+    assert DEFAULT_USERNAME == current_user.name
 
 
-def test_login_invalid_credentials(client):
-    resp = client.post(url_for('login'), data={'username': USERNAME,
+@create_user
+def test_login_invalid_credentials(db, client):
+    resp = client.post(url_for('login'), data={'username': DEFAULT_USERNAME,
                                                'password': 'N' * TRACKER_PASSWORD_LENGTH_MIN})
     assert_not_logged_in(resp, status_code=Unauthorized.code)
     assert ERROR_INVALID_USERNAME_PASSWORD in resp.data.decode()
 
 
-def test_login_disabled(client, db):
+@create_user(username='deactivated-user-account', active=False)
+def test_login_disabled(db, client):
     username = 'deactivated-user-account'
-    create_user(db, username=username, active=False)
     resp = client.post(url_for('login'), data={'username': username, 'password': username})
     assert_not_logged_in(resp, status_code=Unauthorized.code)
     assert ERROR_ACCOUNT_DISABLED in resp.data.decode()
 
 
-def test_logout(client):
-    resp = client.post(url_for('login'), follow_redirects=True,
-                       data=dict(username=USERNAME, password=USERNAME))
-    assert_logged_in(resp)
+@logged_in
+def test_logout(db, client):
     resp = client.post(url_for('logout'), follow_redirects=True)
     assert_not_logged_in(resp)
