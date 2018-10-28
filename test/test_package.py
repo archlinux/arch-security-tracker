4cde59e25c4ac0ec6c0e8e04020eea2a0459f2e1
Jelle van der Waa <jelle@vdwaa.nl>
2018-10-28 15:22:21 +0100

    test: Add test for show_package view

--- test/test_package.py
+++ test/test_package.py
@@ -28,3 +28,15 @@ def test_show_package_json(db, client):
 def test_show_package_json_not_found(db, client):
     resp = client.get(url_for('tracker.show_package_json', pkgname='foo', suffix='/json'), follow_redirects=True)
     assert NotFound.code == resp.status_code
+
+def test_show_package_not_found(db, client):
+    resp = client.get(url_for('tracker.show_package', pkgname='foo'), follow_redirects=True)
+    assert NotFound.code == resp.status_code
+
+@create_package(name='foo', version='1.2.3-4')
+@create_group(id=DEFAULT_GROUP_ID, packages=['foo'], affected='1.2.3-3', fixed='1.2.3-4')
+@create_advisory(id=DEFAULT_ADVISORY_ID, group_package_id=DEFAULT_GROUP_ID, advisory_type=issue_types[1], reference='https://security.archlinux.org', publication=Publication.published)
+def test_show_package(db, client):
+    resp = client.get(url_for('tracker.show_package', pkgname='foo'), follow_redirects=True)
+    assert 200 == resp.status_code
+    assert 'foo' in resp.data.decode()
